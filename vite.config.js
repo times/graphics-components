864b3b9639f201899b6e1dd92037a365748ca967
nmetitiere-dev <fmetitieri.consultant@newscorp.com>
2025-04-21 15:58:09 -0300

    added css incjected by js plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        cssInjectedByJsPlugin()
+    ],
     build: {
         lib: {
             entry: 'src/lib/components/index.jsx',
